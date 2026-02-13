1f119645cff9dc1de2341a3959042376d8f532d0
Alexandru Ticea <52721522+alexticea@users.noreply.github.com>
2026-02-14 00:05:15 +0200

    Small adjustment to d.ts imports

--- solana-mobile.d.ts
+++ solana-mobile.d.ts
@@ -1,10 +1,10 @@
 declare module '@solana-mobile/mobile-wallet-adapter-protocol-web3js' {
-    import { Connection, PublicKey, Transaction, TransactionVersion, VersionedTransaction } from '@solana/web3.js';
+    import { Transaction, VersionedTransaction, SendOptions } from '@solana/web3.js';
     import { MobileWallet } from '@solana-mobile/mobile-wallet-adapter-protocol';
 
     export interface Web3MobileWallet extends Omit<MobileWallet, 'signTransactions' | 'signAndSendTransactions'> {
         signTransactions<T extends Transaction | VersionedTransaction>(config: { transactions: T[] }): Promise<T[]>;
-        signAndSendTransactions<T extends Transaction | VersionedTransaction>(config: { transactions: T[], minContextSlot?: number }): Promise<string[]>;
+        signAndSendTransactions<T extends Transaction | VersionedTransaction>(config: { transactions: T[], minContextSlot?: number, options?: SendOptions }): Promise<string[]>;
     }
 
     export function transact<R>(callback: (wallet: Web3MobileWallet) => Promise<R>): Promise<R>;
